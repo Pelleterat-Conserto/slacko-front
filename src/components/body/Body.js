3d476454119dbef2e2d9cb300891d342252254be
Miguel Pelleterat <mpelleterat.externe@bedrockstreaming.com>
2022-02-21 18:46:02 +0100

    cleans a little

---
+++ src/components/body/Body.js
@@ -0,0 +1,11 @@
+import { Chat } from './chat/Chat';
+import './Body.css';
+
+export const Body = () => {
+  return (
+    <div className="app-body">
+      <Chat />
+    </div>
+  );
+}
+
---
+++ src/components/head/Head.js
@@ -0,0 +1,10 @@
+import './Head.css';
+
+export const Head = () => {
+  return (
+    <div className="app-head">
+      HEAD
+    </div>
+  );
+}
+
